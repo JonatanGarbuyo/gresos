b2264fe317a72638996ef5576542ec71a9e78ae1
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-10-22 22:44:49 -0300

    categories page created

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,3 +1,34 @@
+import "./styles.css";
+
+const categories = [
+  { id: 1, title: "food" },
+  { id: 2, title: "clothes" },
+  { id: 3, title: "rent" },
+  { id: 4, title: "others" },
+];
+
 export default function Categories() {
-  return <h1>Categories</h1>;
+  return (
+    <main className="page_container">
+      <div className="card_container"></div>
+
+      <div className="resume">
+        <h2 className="resume_title">Categories</h2>
+        <ul className="detail detail_container">
+          <div className="detail cell_header detail_row">
+            <li className="cell ">id</li>
+            <li className="cell ">category</li>
+          </div>
+          {categories.map(({ id, title }) => {
+            return (
+              <div className="detail detail_row" key={id}>
+                <li className="cell">{id}</li>
+                <li className="cell">{title}</li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </main>
+  );
 }
