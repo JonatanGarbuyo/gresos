eb0dd817fe5823cc4051d05c11e17ba40af5add0
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-10-24 22:31:12 -0300

    refactor validate resource middleware

--- api/middleware/validateResourceMW.js
+++ api/middleware/validateResourceMW.js
@@ -11,15 +11,27 @@
 //   }
 // };
 
+// const validateResourceMW = (resourceSchema) => (req, res, next) => {
+//   const resource = req.body;
+//   try {
+//     resourceSchema.validate(resource);
+//     next();
+//   } catch (e) {
+//     console.error(e);
+//     res.status(400).json({ error: e.errors.join(", ") });
+//   }
+// };
+
 const validateResourceMW = (resourceSchema) => (req, res, next) => {
   const resource = req.body;
-  try {
-    resourceSchema.validate(resource);
-    next();
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({ error: e.errors.join(", ") });
-  }
+  console.log("resource: ", resource);
+  resourceSchema
+    .validate(resource)
+    .then(() => next())
+    .catch((e) => {
+      console.error(e);
+      res.status(400).json({ error: e.errors.join(", ") });
+    });
 };
 
 export default validateResourceMW;
