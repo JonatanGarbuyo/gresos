f2b69f5132d31475ef8fb6f17a0fd86a6fe658ae
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-10-28 21:44:12 -0300

    add database variables

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+import {} from "dotenv/config";
 import express from "express";
 import cors from "cors";
 
@@ -9,6 +10,7 @@ import transactionsRouter from "./routes/transactions.js";
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logger);
 
@@ -32,7 +34,7 @@ app.use((req, res) => {
     .end();
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
