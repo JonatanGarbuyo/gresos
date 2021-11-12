002efd149adfbf5e3fde279d44cf00154f80f241
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-11-12 14:36:31 -0300

    add handleErrors middleware

---
+++ api/middleware/handleErrors.js
@@ -0,0 +1,17 @@
+const ERROR_HANDLERS = {
+  JsonWebTokenError: (res) =>
+    res.status(401).json({ error: "token missing or invalid" }),
+
+  TokenExpirerError: (res) => res.status(401).json({ error: "token expired" }),
+
+  defaultError: (res, error) => {
+    console.error(error.name);
+    res.status(500).end();
+  },
+};
+
+module.exports = (error, request, response, next) => {
+  const handler = ERROR_HANDLERS[error.name] || ERROR_HANDLERS.defaultError;
+
+  handler(response, error);
+};
